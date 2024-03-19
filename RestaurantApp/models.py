0088302e0ff809c0f6276571a64c19cccc5a0ca9
Shailendra Kumar <shailendrapal840@gmail.com>
2024-03-19 23:12:02 +0530

    Added Slot Booking

--- RestaurantApp/models.py
+++ RestaurantApp/models.py
@@ -3,12 +3,11 @@ from django.db import models
 
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    guest_number = models.IntegerField()
-    comments = models.CharField(max_length=1000)
     reservation_date = models.DateField()
-    def __str__(self):
-        return self.first_name + ' ' + self.last_name
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
     
     
 class Menu(models.Model):
--- RestaurantApp/urls.py
+++ RestaurantApp/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
     path('menu/', views.menu, name="menu"),
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
-    path('bookings/', views.bookings, name="bookings"),
+    path('bookings', views.bookings, name='bookings'), 
 ]
